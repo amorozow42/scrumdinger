9e005785a2606030493c7c1cfb00ae6b35a827d7
Alexey Morozov <losaciertos@icloud.com>
2025-02-21 18:29:47 +0300

    Display history with transcription

---
+++ Scrumdinger/Views/HistoryView.swift
@@ -0,0 +1,46 @@
+//
+//  HistoryView.swift
+//  Scrumdinger
+//
+//  Created by Aleksei Morozow on 21.02.2025.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    let history: History
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Divider()
+                    .padding(.bottom)
+                Text("Attendees")
+                    .font(.headline)
+                Text(history.attendeeString)
+                Text("Transcript")
+                    .font(.headline)
+                    .padding(.top)
+                Text(history.transcript)
+            }
+        }
+        .navigationTitle(Text(history.date, style: .date))
+        .padding()
+    }
+}
+
+#Preview {
+    let history: History =
+            History(
+                attendees: [
+                    DailyScrum.Attendee(name: "Jon"),
+                    DailyScrum.Attendee(name: "Darla"),
+                    DailyScrum.Attendee(name: "Luis")
+                ],
+                transcript: "Darla, would you like to start today? Sure, yesterday I reviewed Luis' PR and met with the design team to finalize the UI..."
+            )
+    
+    NavigationStack {
+        HistoryView(history: history)
+    }
+}
